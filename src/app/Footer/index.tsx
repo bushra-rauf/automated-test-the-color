5d7fb757c717d56e37978e8048d086ba82a150c2
bushra-rauf <bush_rauf@yahoo.com>
2025-08-22 10:18:38 +0200

    added footer

---
+++ src/app/Footer/index.tsx
@@ -0,0 +1,7 @@
+const Footer = () => {
+    return(
+        <footer className="flex justify-center p-10 bg-red-300" >vic: 2025</footer>
+    )
+}
+
+export default Footer
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 import Image from "next/image";
+import Footer from "./Footer";
 
 export default function Home() {
   return (
-    <></>
+    <>
+    <Footer/>
+    </>
   );
 }
